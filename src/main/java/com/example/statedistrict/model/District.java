79f99b5659edfa377d14342b2787b7e43078334c
Rahul Gupta <rahuldevit2000@gmail.com>
2026-04-05 14:17:32 +0530

    //District Bean
    //District Bean

--- src/main/java/com/example/statedistrict/model/District.java
+++ src/main/java/com/example/statedistrict/model/District.java
@@ -5,7 +5,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-
+//District Bean
 @Entity
 @Table(name="district")
 public class District {
